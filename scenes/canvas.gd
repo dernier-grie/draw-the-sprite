34b97ef8970336cc6a7577c30b5f778afaba6b73
Gabriele Corti <dernier.grie@gmail.com>
2026-01-31 17:45:28 +0100

    draw multiple lines

--- scenes/canvas.gd
+++ scenes/canvas.gd
@@ -6,22 +6,32 @@ class_name DrawCanvas
 @export var line_color: Color = Color.DIM_GRAY
 @export var line_distance_min = 5.0
 
-var current_line: PackedVector2Array = PackedVector2Array()
 var is_drawing: bool
 var last_point: Vector2 = Vector2.ZERO
 
+var current_line: PackedVector2Array
+var lines: Array[PackedVector2Array]
+
 func _ready() -> void:
+	current_line = PackedVector2Array()
+	lines = []
 	pass
 
 func _draw() -> void:
 	if current_line.size() > 1:
 		draw_polyline(current_line, line_color, line_width)
+	
+	for line in lines:
+		draw_polyline(line, line_color, line_width)
+		
 		
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		is_drawing = event.is_pressed()
 		
 		if not is_drawing and current_line.size() > 1:
+			last_point = Vector2.ZERO
+			lines.append(current_line)
 			current_line = PackedVector2Array()
 	
 	if is_drawing and (event is InputEventMouseMotion):
