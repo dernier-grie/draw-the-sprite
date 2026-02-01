c18613172a9aa7ccdff1e8be09564318599ef5d6
Gabriele Corti <dernier.grie@gmail.com>
2026-02-01 18:32:41 +0100

    update touch hint
    avoid fade in flashes; make hiding logic private; add a function forcing the hiding from outside if need be

--- scenes/touch_hint/touch_hint.gd
+++ scenes/touch_hint/touch_hint.gd
@@ -4,17 +4,25 @@ class_name TouchHint
 @export var auto_hide_time: float = 3.0
 @onready var timer: Timer = $Timer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+var _fully_visible: bool
 
 func _ready() -> void:
 	timer.wait_time = auto_hide_time
 	timer.one_shot = true
-	timer.timeout.connect(hide_hint)
+	timer.timeout.connect(_hide_hint)
 	show_hint()
 
 func show_hint() -> void:
-	animation_player.play("fade_in")
+	if _fully_visible:
+		timer.start()
+		return
+
+	_fully_visible = true
+	if animation_player.current_animation != "fade_in":
+		animation_player.play("fade_in")
 	timer.start()
 
-func hide_hint() -> void:
+func _hide_hint() -> void:
+	_fully_visible = false
 	animation_player.play("fade_out")
 	
