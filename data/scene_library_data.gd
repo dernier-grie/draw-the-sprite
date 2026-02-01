07164ac4cb8aa917132d7766360e5f25b656d009
Gabriele Corti <dernier.grie@gmail.com>
2026-02-01 11:46:40 +0100

    scene library data
    add animals to subfolder

---
+++ data/scene_library_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name SceneLibraryData
+
+#@export var title_screen: PackedScene
+
+@export var draw_screen: PackedScene
---
+++ scenes/main.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@onready var scene_container: Node = $SceneContainer
