a1d10b4dd5957f8339e2f8b557088bcd794f4ba6
Gabriele Corti <dernier.grie@gmail.com>
2026-02-01 11:51:40 +0100

    scene manager 1.0

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,3 +1,13 @@
 extends Node
 
+@export var scene_library: SceneLibraryData
 @onready var scene_container: Node = $SceneContainer
+
+func _ready() -> void:
+	change_to(scene_library.draw_screen)
+	
+func change_to(target_scene: PackedScene):
+	for child in scene_container.get_children():
+		child.queue_free()
+	var scene = target_scene.instantiate()
+	scene_container.add_child(scene)
